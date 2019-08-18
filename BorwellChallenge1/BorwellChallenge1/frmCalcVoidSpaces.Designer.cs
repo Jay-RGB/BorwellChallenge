2e24cddb2c56d5686db883440fa29b0e93b2f5f5
Jay-RGB <52334944+Jay-RGB@users.noreply.github.com>
2019-08-18 17:29:09 +0100

    UI Forms
    UI Forms created for Startup, Calculation and Results Forms

---
+++ BorwellChallenge1/BorwellChallenge1/frmCalcVoidSpaces.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace BorwellChallenge1
+{
+    partial class frmCalcVoidSpaces
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.lblWidth = new System.Windows.Forms.Label();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.lblInstructionsHeader = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstructions.Location = new System.Drawing.Point(116, 85);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(421, 60);
+            this.lblInstructions.TabIndex = 19;
+            this.lblInstructions.Text = "Enter the Height and Width of one of the door/windows \r\nand then click \"Submit\" t" +
+    "o save the dimensions. \r\nRepeat for each Door/Window and then click Next.";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(295, 258);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(111, 35);
+            this.btnSubmit.TabIndex = 18;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(492, 366);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(111, 35);
+            this.btnBack.TabIndex = 17;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(609, 366);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(111, 35);
+            this.btnNext.TabIndex = 16;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            // 
+            // lblWidth
+            // 
+            this.lblWidth.AutoSize = true;
+            this.lblWidth.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWidth.Location = new System.Drawing.Point(116, 195);
+            this.lblWidth.Name = "lblWidth";
+            this.lblWidth.Size = new System.Drawing.Size(52, 20);
+            this.lblWidth.TabIndex = 15;
+            this.lblWidth.Text = "Width";
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeight.Location = new System.Drawing.Point(116, 167);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(58, 20);
+            this.lblHeight.TabIndex = 14;
+            this.lblHeight.Text = "Height";
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(255, 192);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(192, 22);
+            this.txtWidth.TabIndex = 13;
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(255, 164);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(192, 22);
+            this.txtHeight.TabIndex = 12;
+            // 
+            // lblInstructionsHeader
+            // 
+            this.lblInstructionsHeader.AutoSize = true;
+            this.lblInstructionsHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstructionsHeader.Location = new System.Drawing.Point(80, 50);
+            this.lblInstructionsHeader.Name = "lblInstructionsHeader";
+            this.lblInstructionsHeader.Size = new System.Drawing.Size(491, 25);
+            this.lblInstructionsHeader.TabIndex = 11;
+            this.lblInstructionsHeader.Text = "Please input the dimensions of each Door and Window:";
+            // 
+            // frmCalcVoidSpaces
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblInstructions);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.lblWidth);
+            this.Controls.Add(this.lblHeight);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.lblInstructionsHeader);
+            this.Name = "frmCalcVoidSpaces";
+            this.Text = "Borwell Challenge - Paint Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Label lblInstructions;
+        internal System.Windows.Forms.Button btnSubmit;
+        internal System.Windows.Forms.Button btnBack;
+        internal System.Windows.Forms.Button btnNext;
+        internal System.Windows.Forms.Label lblWidth;
+        internal System.Windows.Forms.Label lblHeight;
+        internal System.Windows.Forms.TextBox txtWidth;
+        internal System.Windows.Forms.TextBox txtHeight;
+        internal System.Windows.Forms.Label lblInstructionsHeader;
+    }
+}
---
+++ BorwellChallenge1/BorwellChallenge1/frmResults.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace BorwellChallenge1
+{
+    partial class frmResults
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPaintWastageResult = new System.Windows.Forms.Label();
+            this.lblPaintResult = new System.Windows.Forms.Label();
+            this.lblVolumeResult = new System.Windows.Forms.Label();
+            this.lblAreaResult = new System.Windows.Forms.Label();
+            this.lblWallPaint = new System.Windows.Forms.Label();
+            this.lblRoomVolume = new System.Windows.Forms.Label();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.lblWallPaintWastage = new System.Windows.Forms.Label();
+            this.lblFloorArea = new System.Windows.Forms.Label();
+            this.lblResultsHeader = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblPaintWastageResult
+            // 
+            this.lblPaintWastageResult.AutoSize = true;
+            this.lblPaintWastageResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPaintWastageResult.Location = new System.Drawing.Point(499, 202);
+            this.lblPaintWastageResult.Name = "lblPaintWastageResult";
+            this.lblPaintWastageResult.Size = new System.Drawing.Size(0, 20);
+            this.lblPaintWastageResult.TabIndex = 25;
+            // 
+            // lblPaintResult
+            // 
+            this.lblPaintResult.AutoSize = true;
+            this.lblPaintResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPaintResult.Location = new System.Drawing.Point(530, 152);
+            this.lblPaintResult.Name = "lblPaintResult";
+            this.lblPaintResult.Size = new System.Drawing.Size(0, 20);
+            this.lblPaintResult.TabIndex = 24;
+            // 
+            // lblVolumeResult
+            // 
+            this.lblVolumeResult.AutoSize = true;
+            this.lblVolumeResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVolumeResult.Location = new System.Drawing.Point(391, 123);
+            this.lblVolumeResult.Name = "lblVolumeResult";
+            this.lblVolumeResult.Size = new System.Drawing.Size(0, 20);
+            this.lblVolumeResult.TabIndex = 23;
+            // 
+            // lblAreaResult
+            // 
+            this.lblAreaResult.AutoSize = true;
+            this.lblAreaResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAreaResult.Location = new System.Drawing.Point(362, 94);
+            this.lblAreaResult.Name = "lblAreaResult";
+            this.lblAreaResult.Size = new System.Drawing.Size(0, 20);
+            this.lblAreaResult.TabIndex = 22;
+            // 
+            // lblWallPaint
+            // 
+            this.lblWallPaint.AutoSize = true;
+            this.lblWallPaint.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWallPaint.Location = new System.Drawing.Point(116, 152);
+            this.lblWallPaint.Name = "lblWallPaint";
+            this.lblWallPaint.Size = new System.Drawing.Size(381, 20);
+            this.lblWallPaint.TabIndex = 21;
+            this.lblWallPaint.Text = "The Amount of Paint required to paint the walls is:";
+            // 
+            // lblRoomVolume
+            // 
+            this.lblRoomVolume.AutoSize = true;
+            this.lblRoomVolume.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRoomVolume.Location = new System.Drawing.Point(116, 123);
+            this.lblRoomVolume.Name = "lblRoomVolume";
+            this.lblRoomVolume.Size = new System.Drawing.Size(217, 20);
+            this.lblRoomVolume.TabIndex = 20;
+            this.lblRoomVolume.Text = "The Volume of the Room is:";
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(492, 366);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(111, 35);
+            this.btnBack.TabIndex = 19;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Location = new System.Drawing.Point(609, 366);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(111, 35);
+            this.btnRestart.TabIndex = 18;
+            this.btnRestart.Text = "Restart";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            // 
+            // lblWallPaintWastage
+            // 
+            this.lblWallPaintWastage.AutoSize = true;
+            this.lblWallPaintWastage.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWallPaintWastage.Location = new System.Drawing.Point(116, 182);
+            this.lblWallPaintWastage.Name = "lblWallPaintWastage";
+            this.lblWallPaintWastage.Size = new System.Drawing.Size(363, 40);
+            this.lblWallPaintWastage.TabIndex = 17;
+            this.lblWallPaintWastage.Text = "The Amount of Paint required to paint the walls \r\nwith a 10% allowance for wastag" +
+    "e is:";
+            // 
+            // lblFloorArea
+            // 
+            this.lblFloorArea.AutoSize = true;
+            this.lblFloorArea.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFloorArea.Location = new System.Drawing.Point(116, 94);
+            this.lblFloorArea.Name = "lblFloorArea";
+            this.lblFloorArea.Size = new System.Drawing.Size(190, 20);
+            this.lblFloorArea.TabIndex = 16;
+            this.lblFloorArea.Text = "The Area of the Floor is:";
+            // 
+            // lblResultsHeader
+            // 
+            this.lblResultsHeader.AutoSize = true;
+            this.lblResultsHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultsHeader.Location = new System.Drawing.Point(80, 50);
+            this.lblResultsHeader.Name = "lblResultsHeader";
+            this.lblResultsHeader.Size = new System.Drawing.Size(82, 25);
+            this.lblResultsHeader.TabIndex = 15;
+            this.lblResultsHeader.Text = "Results:";
+            // 
+            // frmResults
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblPaintWastageResult);
+            this.Controls.Add(this.lblPaintResult);
+            this.Controls.Add(this.lblVolumeResult);
+            this.Controls.Add(this.lblAreaResult);
+            this.Controls.Add(this.lblWallPaint);
+            this.Controls.Add(this.lblRoomVolume);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.lblWallPaintWastage);
+            this.Controls.Add(this.lblFloorArea);
+            this.Controls.Add(this.lblResultsHeader);
+            this.Name = "frmResults";
+            this.Text = "Borwell Challenge - Paint Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Label lblPaintWastageResult;
+        internal System.Windows.Forms.Label lblPaintResult;
+        internal System.Windows.Forms.Label lblVolumeResult;
+        internal System.Windows.Forms.Label lblAreaResult;
+        internal System.Windows.Forms.Label lblWallPaint;
+        internal System.Windows.Forms.Label lblRoomVolume;
+        internal System.Windows.Forms.Button btnBack;
+        internal System.Windows.Forms.Button btnRestart;
+        internal System.Windows.Forms.Label lblWallPaintWastage;
+        internal System.Windows.Forms.Label lblFloorArea;
+        internal System.Windows.Forms.Label lblResultsHeader;
+    }
+}
